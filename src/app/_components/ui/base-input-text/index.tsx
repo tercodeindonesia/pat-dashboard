f97039362ea49eb6dfc2310b9c30de8855f92e60
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2025-06-10 21:36:41 +0700

    feat: components

---
+++ src/app/_components/ui/base-input-text/index.tsx
@@ -0,0 +1,21 @@
+import { TextField as MuiTextField, styled, TextFieldProps } from "@mui/material";
+
+const BaseInput = styled(MuiTextField)(() => ({
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-disabled": {
+      "& .MuiOutlinedInput-notchedOutline": {
+        border: "none",
+      },
+      backgroundColor: "#F0F0F0",
+    },
+  },
+  "& .MuiFormHelperText-root": {
+    marginLeft: 10,
+  },
+}));
+
+const BaseInputText = (props: TextFieldProps) => {
+  return <BaseInput {...props} variant="outlined" />;
+};
+
+export default BaseInputText;
---
+++ src/app/_components/ui/form-text-field/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  FormControl,
+  FormGroup,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Switch,
+  Typography,
+} from "@mui/material";
+import { Controller, FieldValues, UseControllerProps } from "react-hook-form";
+
+import BaseInputText from "../base-input-text";
+
+type Props<T extends FieldValues> = UseControllerProps<T> &
+  React.ComponentProps<typeof BaseInputText> & {
+    label?: string;
+    required?: boolean;
+    endLink?: boolean;
+    handleToggle?: () => void;
+    toogle?: boolean;
+  };
+
+const FormInputTextField = <T extends FieldValues>({
+  control,
+  name,
+  label = "",
+  required = false,
+  placeholder = "",
+  endLink = false,
+  handleToggle = () => {},
+  toogle = false,
+  type = "text",
+  ...rest
+}: Props<T>) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => (
+        <FormControl variant="standard" sx={{ width: "100%" }}>
+          {label ? (
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <InputLabel htmlFor={field.name} error={fieldState.invalid} required={required}>
+                {label}
+              </InputLabel>
+              {endLink ? (
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <Typography variant="caption" mt={1}>
+                    Arahkan Ke Whatapps
+                  </Typography>
+                  <Switch
+                    checked={toogle}
+                    onChange={handleToggle}
+                    color="primary"
+                    slotProps={{
+                      input: { "aria-label": "toggle on off" },
+                    }}
+                  />
+                </div>
+              ) : null}
+            </div>
+          ) : null}
+          <FormGroup>
+            <BaseInputText
+              {...rest}
+              variant="outlined"
+              id={field.name}
+              value={field.value}
+              defaultValue={rest.defaultValue || ""}
+              onChange={field.onChange}
+              placeholder={placeholder}
+              error={fieldState.invalid}
+              helperText={fieldState.error?.message}
+              type={type === "password" ? (!showPassword ? "password" : "text") : type}
+              slotProps={{
+                input:
+                  type === "password"
+                    ? {
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton onClick={togglePasswordVisibility} edge="end">
+                              {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }
+                    : undefined,
+              }}
+            />
+          </FormGroup>
+        </FormControl>
+      )}
+    />
+  );
+};
+
+export default FormInputTextField;
